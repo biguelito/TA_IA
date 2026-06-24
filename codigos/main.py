4a03e5c9500934a46b2d8bcf0d069829b7016bb3
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2026-06-24 16:52:59 -0300

    ajustando

--- codigos/main.py
+++ codigos/main.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
  
     solver.run_complete_centroid_experiment(
         instance_variations=["eil51", "berlin52_1", "berlin52_2", "eil76_1", "eil76_2", "rat99"],
-        final_runs=30
+        final_runs=30,
         save_all=True
     )
