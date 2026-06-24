bcfe83475a4bda93634a751090afec84ee4cc796
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2026-06-24 16:52:26 -0300

    iniciando maior massa de teste

--- codigos/main.py
+++ codigos/main.py
@@ -4,9 +4,6 @@ if __name__ == "__main__":
     solver = Solver()
  
     solver.run_complete_centroid_experiment(
-        instance_variations=["eil51"],
-        screening_values=[0.20, 0.80],
-        screening_runs=2,
-        final_runs=2,
-        base_seed=42,
+        instance_variations=["eil51", "berlin52_1", "berlin52_2", "eil76_1", "eil76_2", "rat99"],
+        save_all=True
     )
