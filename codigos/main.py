d0c898078efb9590cb9fcc6269bcc01435c2c652
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2026-06-24 17:13:58 -0300

    realizando teste maior

--- codigos/main.py
+++ codigos/main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
  
     solver.run_complete_centroid_experiment(
         instance_variations=["eil51", "berlin52_1", "berlin52_2", "eil76_1", "eil76_2", "rat99"],
-        final_runs=30,
-        save_all=True
+        final_runs=50,
+        save_all=True,
+        base_seed=69420
     )
